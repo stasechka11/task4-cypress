186d855cdda73d58fac7b6660e34badde5fe4cad
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-06-20 14:26:22 +0400

    added command login

--- cypress/e2e/book_app_test.cy.js
+++ cypress/e2e/book_app_test.cy.js
@@ -1,10 +1,7 @@
-describe('Login to BookApp', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('.ml-auto > .ml-2').click()
-    cy.get('#mail').type("bropet@mail.ru")
-    cy.get('#pass').type("123")
-    cy.contains("Submit").click()
-    cy.contains("Добро пожаловать bropet@mail.ru").should("be.visible")
+describe('Tests BooksApp', () => {
+  it('Success login', () => {
+    cy.visit('http://localhost:3000/');
+    cy.login("bropet@mail.ru", 123);
+    cy.contains("Добро пожаловать bropet@mail.ru").should("be.visible");
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,15 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (email, password) => { 
+    cy.get('.ml-auto > .ml-2').click();
+    
+    if (email) 
+        cy.get('#mail').type(email);
+
+    if (password) 
+        cy.get('#pass').type(password);
+
+    cy.contains("Submit").click();
+ })
