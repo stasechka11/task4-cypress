908a1434fe635c35d440ba55bb953e94c2958bc1
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-06-23 16:26:18 +0400

    Moved visit site to beforeEach

--- cypress/e2e/book_app_test.cy.js
+++ cypress/e2e/book_app_test.cy.js
@@ -1,24 +1,24 @@
 describe("Tests BooksApp", () => {
-  it.skip("Success login", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000/");
+  })
+
+  it("Success login", () => {
     cy.login("bropet@mail.ru", 123);
     cy.contains("Добро пожаловать bropet@mail.ru").should("be.visible");
   })
 
-  it.skip("Login with empty email", () => {
-    cy.visit("http://localhost:3000/");
+  it("Login with empty email", () => {
     cy.login("", 123);
     cy.CheckInputValidity("#mail");
   })
 
-  it.skip("Login with empty password", () => {
-    cy.visit("http://localhost:3000/");
+  it("Login with empty password", () => {
     cy.login("bropet@mail.ru", "");
     cy.CheckInputValidity("#pass");
   })
 
   it("Login with empty email and password", () => {
-    cy.visit("http://localhost:3000/");
     cy.login("", "");
     cy.CheckInputValidity("#mail");
     cy.CheckInputValidity("#pass");
