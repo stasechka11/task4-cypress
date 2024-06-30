36ed3235ccbc059dd1bf6f14f8897771b4e64ac9
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-06-30 12:42:38 +0400

    add to favorities test

--- cypress/e2e/book_app_test.cy.js
+++ cypress/e2e/book_app_test.cy.js
@@ -33,4 +33,16 @@ describe("Tests BooksApp", () => {
     cy.get('.card-body > .card-title').last().should("have.text", title);
     cy.get('.card-body > .card-text').last().should("have.text", author);
   })
+
+  it("Add book to favorites", () => {
+    const title = "Title" + Date.now();
+    const author = "Author" + Date.now();
+
+    cy.login("bropet@mail.ru", 123);
+    cy.addBook(title, author);
+    cy.contains('.card-title', title).parent().parent().contains('button', 'Add to favorite').click();
+    cy.contains('.card-title', title).parent().parent().contains('button', 'Delete from favorite').should('be.visible');
+    cy.get('[href="/favorites"]').click();
+    cy.contains('.card-title', title).should('be.visible');
+  })
 })
