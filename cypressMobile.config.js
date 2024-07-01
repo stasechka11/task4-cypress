b0cf8a474321f6f8ad22199aa4cbc5bf175d283d
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-07-01 13:29:03 +0400

    Added different configurations and scripts

--- cypressMobile.config.js
+++ cypressMobile.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl : 'http://localhost:3000/',
-    viewportWidth: 1920,
-    viewportHeight: 1080,
+    viewportWidth: 360,
+    viewportHeight: 800,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
